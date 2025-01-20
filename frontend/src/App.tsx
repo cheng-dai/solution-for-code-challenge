14f49b6459d83b86651d1a21dc8d11c5eeb20619
chengdai <chengdai.dev@gmail.com>
2025-01-20 21:39:09 +0100

    Add first chart

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import "chart.js/auto";
 import { Chart } from "react-chartjs-2";
+
 import { fetchFundingRounds } from "./api";
+import { ChartOptions } from "chart.js";
 
 const App = () => {
   const [fundingData, setFundingData] = useState<
-    { name: string; amount: number }[]
+    { name: string; amount: number; date: Date }[]
   >([]);
 
   useEffect(() => {
@@ -20,13 +22,60 @@ const App = () => {
 
     loadFundingData();
   }, []);
+  // chart one
+  // chart one options
+  const chartOneOptions: ChartOptions = {
+    scales: {
+      y: {
+        beginAtZero: false,
+      },
+    },
+    plugins: {
+      title: {
+        display: true,
+        text: "Top 15 Funding Rounds",
+        font: {
+          size: 20,
+        },
+      },
+      tooltip: {
+        callbacks: {
+          label: function (context) {
+            const dataset = context.dataset;
+            const dataIndex = context.dataIndex;
+            const value = dataset.data[dataIndex];
+            // There currently is a bug in Prisma, DateTime value need to be transformed to a Date object before pass in as string. https://github.com/prisma/prisma/issues/9516
+            const date = new Date(biggestFundingData[dataIndex].date);
+            return [`Amount: ${value}`, `Date: ${date.toLocaleDateString()}`];
+          },
+        },
+      },
+    },
+  };
+  // chart one data
+  const biggestFundingData = [...fundingData]
+    .sort((a, b) => b.amount - a.amount)
+    .slice(0, 15);
 
-  const chartData = {
-    labels: fundingData.map(item => item.name),
+  // const chartData = {
+  //   labels: fundingData.map(item => item.name),
+  //   datasets: [
+  //     {
+  //       label: "Funding Amounts",
+  //       data: fundingData.map(item => item.amount),
+  //       backgroundColor: "rgba(75, 192, 192, 0.6)",
+  //       borderColor: "rgba(75, 192, 192, 1)",
+  //       borderWidth: 1,
+  //     },
+  //   ],
+  // };
+
+  const chartOneData = {
+    labels: biggestFundingData.map(item => item.name),
     datasets: [
       {
         label: "Funding Amounts",
-        data: fundingData.map(item => item.amount),
+        data: biggestFundingData.map(item => item.amount),
         backgroundColor: "rgba(75, 192, 192, 0.6)",
         borderColor: "rgba(75, 192, 192, 1)",
         borderWidth: 1,
@@ -55,7 +104,7 @@ const App = () => {
 
       <h1>Example Chart</h1>
       <p>This is a scaffold to get you started.</p>
-      <Chart type="bar" data={chartData} />
+      <Chart type="bar" options={chartOneOptions} data={chartOneData} />
     </div>
   );
 };
