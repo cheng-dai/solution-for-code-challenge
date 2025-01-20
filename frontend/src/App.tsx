c774b5002338c51ec1bcb043b9242d982335742b
chengdai <chengdai.dev@gmail.com>
2025-01-20 22:44:55 +0100

    Remove unused code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -137,43 +137,10 @@ const App = () => {
     ],
   };
 
-  // const chartData = {
-  //   labels: fundingData.map(item => item.name),
-  //   datasets: [
-  //     {
-  //       label: "Funding Amounts",
-  //       data: fundingData.map(item => item.amount),
-  //       backgroundColor: "rgba(75, 192, 192, 0.6)",
-  //       borderColor: "rgba(75, 192, 192, 1)",
-  //       borderWidth: 1,
-  //     },
-  //   ],
-  // };
-
   return (
-    <div style={{ width: "80%", margin: "0 auto", textAlign: "start" }}>
-      <h1>Assignment</h1>
-      <p>Your task is to show two charts on this page:</p>
-      <p>
-        The first chart should show the biggest 15 funding rounds from the DB
-        sorted by amount, showing the round name and date (hint: date
-        information exists in the DB but you may need to adjust prisma schema to
-        use it).
-      </p>
-      <p>
-        The second chart should show the timeline of all funding rounds from the
-        DB using the date for x-axis and the amount for y-axis.
-      </p>
-      <p>
-        The database for this assignment is an SQLite db that you can find in:
-        <pre>backend/prisma/dev.db</pre>
-      </p>
+    <div style={{ width: "90vw", margin: "10px 24px", textAlign: "start" }}>
+      <Chart type="bar" options={chartOneOptions} data={chartOneData} />
 
-      <h1>Example Chart</h1>
-      <p>This is a scaffold to get you started.</p>
-      <div className="chart-container">
-        <Chart type="bar" options={chartOneOptions} data={chartOneData} />
-      </div>
       <Chart type="bar" options={chartTwoOptions} data={chartTwoData} />
     </div>
   );
